1679f518e647d0b5195e026a8058db4b3c959bb7
Udyatbhanu <udyatbhanu@gmail.com>
2020-03-31 11:30:26 -0700

    First commit

---
+++ app/src/main/java/com/swapi/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.swapi.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.swapi.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
