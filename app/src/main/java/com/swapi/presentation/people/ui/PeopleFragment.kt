87f9db29c9ebb9e1d9332d3a190c0dfb16850e2b
Udyatbhanu <udyatbhanu@gmail.com>
2020-04-05 17:04:37 -0700

    Card changes

--- app/src/main/java/com/swapi/presentation/people/ui/PeopleFragment.kt
+++ app/src/main/java/com/swapi/presentation/people/ui/PeopleFragment.kt
@@ -1,19 +1,26 @@
 package com.swapi.presentation.people.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.DefaultItemAnimator
 import com.swapi.R
 import com.swapi.core.dagger.ViewModelFactory
 import com.swapi.databinding.PeopleFragmentBinding
 import com.swapi.presentation.people.viewmodel.PeopleViewModel
 import dagger.android.support.DaggerFragment
+import kotlinx.android.synthetic.main.people_fragment.*
 import javax.inject.Inject
+
 
 class PeopleFragment : DaggerFragment(){
 
@@ -41,6 +48,14 @@ class PeopleFragment : DaggerFragment(){
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         (activity as? AppCompatActivity)?.supportActionBar?.show()
+
+        (activity as? AppCompatActivity)?.setSupportActionBar(toolbar)
+
+        toolbar.setNavigationIcon(R.drawable.ic_action_back)
+        toolbar.setNavigationOnClickListener {
+            Navigation.findNavController(binding.root).popBackStack()
+        }
+
         initViewModels()
         initUi()
         subscribeUi()
@@ -55,7 +70,7 @@ class PeopleFragment : DaggerFragment(){
     private fun subscribeUi() {
         peopleViewModel.getPeople.observe(viewLifecycleOwner, Observer {peopleListItemViewModels ->
             peopleListAdapter = PeopleListAdapter(peopleListItemViewModels)
-            binding.peopleList.adapter = peopleListAdapter
+            peopleList.adapter = peopleListAdapter
 
         })
 
@@ -63,6 +78,20 @@ class PeopleFragment : DaggerFragment(){
 
     private fun initUi() {
         binding.peopleList
+
+        binding.collapsingToolbar.setExpandedTitleColor(Color.TRANSPARENT)
+
+
+        //We could use databinding or ktx extensions
+        peopleList.itemAnimator = DefaultItemAnimator()
+
+
+    }
+
+    private fun initToolbar(){
+
+
+//        toolbar.navigationIcon.onC
     }
 
     private fun initViewModels(){
