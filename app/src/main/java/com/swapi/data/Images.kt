6db560bb971b0941edaeba3b967b5675dcdb26e9
Udyatbhanu <udyatbhanu@gmail.com>
2020-04-07 12:20:11 -0700

    Layout adjustments

---
+++ app/src/main/java/com/swapi/data/Images.kt
@@ -0,0 +1,8 @@
+package com.swapi.data
+
+object Images{
+    const val HOME_IMAGE = "https://swapi-ubbs-cdn.s3-us-west-1.amazonaws.com/swapi-dev/New-Star-Wars-Trilogy.jpg"
+    const val PEOPLE_IMAGE = "https://swapi-ubbs-cdn.s3-us-west-1.amazonaws.com/swapi-dev/people.jpg"
+
+
+}
--- app/src/main/java/com/swapi/presentation/MainFragment.kt
+++ app/src/main/java/com/swapi/presentation/MainFragment.kt
@@ -7,7 +7,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.swapi.R
+import com.swapi.data.Images
 import com.swapi.databinding.MainFragmentBinding
 import dagger.android.support.DaggerFragment
 
@@ -18,6 +23,14 @@ class MainFragment : DaggerFragment() {
 
     private lateinit var binding: MainFragmentBinding
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,22 +38,40 @@ class MainFragment : DaggerFragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.main_fragment, container, false)
 
-
+        initUI()
 
         return binding.root
     }
 
+    private fun initUI() {
 
+        val requestOptions = RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.NONE) // because file name is always same
+            .skipMemoryCache(true)
+
+
+        Glide.with(binding.root.context)
+            .asBitmap()
+            .load(Images.HOME_IMAGE)
+            .centerCrop()
+            .into(binding.homeBannerImage)
+
+
+        Glide.with(binding.root.context)
+            .asBitmap()
+            .load(Images.PEOPLE_IMAGE)
+            .centerCrop()
+            .into(binding.people)
+    }
     /**
      *
      */
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        testButton.setOnClickListener{
-            Navigation.findNavController(binding.root).navigate(R.id.peopleFragment)
-        }
+//        testButton.setOnClickListener{
+//            Navigation.findNavController(binding.root).navigate(R.id.peopleFragment)
+//        }
     }
-
 
 
 }
